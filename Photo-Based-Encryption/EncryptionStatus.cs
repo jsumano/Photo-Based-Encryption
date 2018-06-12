87da47fea44aa6c66781d8402acdc451b1bacca8
Joseph Umano <joeumano@gmail.com>
2018-06-12 17:38:59 -0400

    Create EncryptionStatus enum

---
+++ Photo-Based-Encryption/EncryptionStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo_Based_Encryption
+{
+    enum EncryptionStatus
+    {
+        Idle,
+        Encrypting,
+        Decrypting
+    }
+}
