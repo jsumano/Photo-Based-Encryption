4fcb4928d739cc9e8128c5c54b8c4ff889686b8f
Joseph Umano <joeumano@gmail.com>
2018-06-13 20:03:27 -0400

    Create AES.

--- Photo-Based-Encryption/Encryption.cs
+++ Photo-Based-Encryption/Encryption.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.IO;
 using System.Security.Cryptography;
 using System.Runtime.InteropServices;
 
@@ -17,6 +18,20 @@ namespace Photo_Based_Encryption
             // Generate the salt from random pixel values.
             Bitmap bitmap = new Bitmap(imagePath);
             byte[] salt = PixelReader.GetPixelKey(bitmap);
+
+            // Create a file stream for the output file.
+            FileStream fsOutput = new FileStream(targetFile + ".aes", FileMode.Create);
+
+            // Convert password into an array of bytes.
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            // Create AES.
+            Aes aes = Aes.Create();
+            aes.BlockSize = 128;
+            // Use the maximum keysize.
+            aes.KeySize = 256;
+            // Set the padding mode to PKCS7 which pads using bytes equal to the total number of padding bytes.
+            aes.Padding = PaddingMode.PKCS7;
         }
     }
 }
