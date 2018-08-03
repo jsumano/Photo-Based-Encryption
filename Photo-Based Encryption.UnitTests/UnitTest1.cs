8e1e2a1928342686227e60f9e215d2cc0f44b422
Joseph Umano <joeumano@gmail.com>
2018-08-02 21:01:37 -0400

    Create unit test project.

---
+++ Photo-Based Encryption.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Photo_Based_Encryption.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
